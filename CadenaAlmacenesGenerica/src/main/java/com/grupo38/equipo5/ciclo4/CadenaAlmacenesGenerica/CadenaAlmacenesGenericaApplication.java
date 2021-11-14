687b9afe556614c5b553b1fb071200a48f994e71
jhonksas <37823222+AlexKsas@users.noreply.github.com>
2021-11-14 10:07:35 -0500

    back summit

---
+++ CadenaAlmacenesGenerica/src/main/java/com/grupo38/equipo5/ciclo4/CadenaAlmacenesGenerica/CadenaAlmacenesGenericaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo38.equipo5.ciclo4.CadenaAlmacenesGenerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadenaAlmacenesGenericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadenaAlmacenesGenericaApplication.class, args);
+	}
+
+}
---
+++ CadenaAlmacenesGenerica/src/test/java/com/grupo38/equipo5/ciclo4/CadenaAlmacenesGenerica/CadenaAlmacenesGenericaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo38.equipo5.ciclo4.CadenaAlmacenesGenerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadenaAlmacenesGenericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
