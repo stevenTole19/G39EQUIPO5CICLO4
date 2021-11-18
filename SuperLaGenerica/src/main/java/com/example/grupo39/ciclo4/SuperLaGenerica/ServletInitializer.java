fbdccd9e21d4ca9211cb3841a351aa5e76e9e51a
Sebasml90 <90942700+Sebasml90@users.noreply.github.com>
2021-11-17 21:11:16 -0500

    v1 usuarios
    Prueba

---
+++ SuperLaGenerica/src/main/java/com/example/grupo39/ciclo4/SuperLaGenerica/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.grupo39.ciclo4.SuperLaGenerica;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SuperLaGenericaApplication.class);
+	}
+
+}
---
+++ SuperLaGenerica/src/main/java/com/example/grupo39/ciclo4/SuperLaGenerica/modelo/Venta.java
@@ -0,0 +1,8 @@
+package com.example.grupo39.ciclo4.SuperLaGenerica.modelo;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "ventas")
+public class Venta {
+
+}
