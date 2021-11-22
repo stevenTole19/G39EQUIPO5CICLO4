b3c0fcf7bc48adb4b6b104cf25d40ace049aedb3
jhonksas <74794245+jhonksas@users.noreply.github.com>
2021-11-21 21:58:26 -0500

    delete proyect

--- SuperLaGenerica/src/main/java/com/example/grupo39/ciclo4/SuperLaGenerica/modelo/Venta.java
+++
@@ -1,8 +0,0 @@
-package com.example.grupo39.ciclo4.SuperLaGenerica.modelo;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "ventas")
-public class Venta {
-
-}
